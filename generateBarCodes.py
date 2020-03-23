3eadb4122370146f4727ca6bd9ade9fc6d72abbf
Fejes <gergo.fejes@evopro-group.com>
2020-03-23 16:38:42 +0100

    every 5th element is 'multiple Code' and every 6th element is 'no read'

--- generateBarCodes.py
+++ generateBarCodes.py
@@ -6,12 +6,20 @@
 # MULTIPLE CODE:
 # "!!!!!!!!!!  A00000000000000"
 
+noRead = '"??????????  200000000000000"'
+multipleCode = '"!!!!!!!!!!  A00000000000000"'
+
 
 telegramArray = ""
 startingCode = 1111111111
 
 for i in range(255) :
-    currentCodeTelegram = '"??????????  ;' + str(startingCode + i) + '  00"'
+    if i % 5 == 0 :
+        currentCodeTelegram = multipleCode
+    elif i % 6 == 0 :
+        currentCodeTelegram = noRead
+    else:
+        currentCodeTelegram = '"??????????  ;' + str(startingCode + i) + '  00"'
     if i != 254 :
         telegramArray += currentCodeTelegram + ', \n'
     else :
