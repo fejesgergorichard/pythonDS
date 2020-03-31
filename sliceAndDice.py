6c09d705cc030e9c8d3f4a21ddfdbcc214a3c4f4
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-04-01 00:09:47 +0200

    list-based update of labels

--- sliceAndDice.py
+++ sliceAndDice.py
@@ -6,43 +6,71 @@ top = tkinter.Tk()
 
 
 var = [
-        [StringVar(), StringVar(), StringVar()],
-        [StringVar(), StringVar(), StringVar()]
+        [StringVar(), StringVar(), StringVar(), StringVar()],
+        [StringVar(), StringVar(), StringVar(), StringVar()],
+        [StringVar(), StringVar(), StringVar(), StringVar()]
     ]
 
-a = 48
-b = 49
-c = 50
-for row in var:
-    for entry in row:
-        entry.set(chr(a))
-
-label_0 = Label(top, textvariable=var[1][1], relief=RAISED).grid(row=0, sticky=W, )
-label_01 = Label(top, textvariable=var[0][1], relief=RAISED).grid(row = 0, column = 1, sticky = W)
-label_1 = Label(top, textvariable=var[1][2], relief=RAISED ).grid(row = 1, sticky = W)
-label_2 = Label(top, textvariable=var[1][0], relief=RAISED ).grid(row = 1, column = 1, sticky = E)
-
 labels = [
-            [Label(top, textvariable=var[0], relief=RAISED), Label(top, textvariable=var[0], relief=RAISED), Label(top, textvariable=var[0], relief=RAISED), Label(top, textvariable=var[0], relief=RAISED)],
-            [Label(top, textvariable=var[0], relief=RAISED), Label(top, textvariable=var[0], relief=RAISED), Label(top, textvariable=var[0], relief=RAISED), Label(top, textvariable=var[0], relief=RAISED)]
+            [Label(top, relief=RAISED, textvariable = var[0][0]), Label(top, relief=RAISED, textvariable = var[0][1]), Label(top, relief=RAISED, textvariable = var[0][2]), Label(top, relief=RAISED, textvariable = var[0][3])],
+            [Label(top, relief=RAISED, textvariable = var[1][0]), Label(top, relief=RAISED, textvariable = var[1][1]), Label(top, relief=RAISED, textvariable = var[1][2]), Label(top, relief=RAISED, textvariable = var[1][3])],
+            [Label(top, relief=RAISED, textvariable = var[2][0]), Label(top, relief=RAISED, textvariable = var[2][1]), Label(top, relief=RAISED, textvariable = var[2][2]), Label(top, relief=RAISED, textvariable = var[2][3])]    
         ]
 
-var[1][1].set("fasz")
+labelsValues = [[48,49,50,51],[52,53,54,55],[70,71,72,73,74]]
 
-def helloCallBack():
+
+i = 0
+for row in var:
+    j = 0
+    for item in row:
+        item.set(chr(labelsValues[i][j]))
+        print(item.get())
+        j += 1
+    i += 1
+
+
+j = 1
+for lists in labels:
+    i = 0
+    for label in lists:
+        label.grid(row = i, column = j)
+        label.textvariable = var[j-1][i]
+        print(label.textvariable)
+        i += 1
+    j += 1
+
+def incrementList(oldValues, inc = 1):
+    """Increments the integers stored in a given list by inc, which is 1 by default"""
+    newValues = oldValues
+    i = 0
+    for row in oldValues:
+        j = 0
+        for entry in row:
+            newValues[i][j] = entry + inc
+            j += 1
+        i += 1
+    return newValues
+
+
+def buttonFunction():
     #messagebox.showinfo( "Updated.")
-    global a,b,c
-    a += 1
-    b += 1
-    c += 1
-    var[0][1].set(chr(a))
-    var[0][1].set(chr(b))
-    var[0][2].set(chr(c))
+    global var
+    global labelsValues
+
+    labelsValues = incrementList(labelsValues, 1)
+
+    i = 0
+    for row in var:
+        j = 0
+        for item in row:
+            item.set(chr(labelsValues[i][j]))
+            print(item.get())
+            j += 1
+        i += 1
 
 
-B = tkinter.Button(top, text ="Next", command = helloCallBack)
+B = tkinter.Button(top, text ="Next", command = buttonFunction)
 
-
-
-B.grid(row = 4)
+B.grid(row = i, column = 2)
 top.mainloop()
---
+++ updateList.py
@@ -0,0 +1,64 @@
+import tkinter
+from tkinter import *
+import tkinter.messagebox as messagebox
+
+top = tkinter.Tk()
+
+
+var = [
+        [StringVar(), StringVar(), StringVar(), StringVar()],
+        [StringVar(), StringVar(), StringVar(), StringVar()]
+    ]
+
+labels = [
+            [Label(top, relief=RAISED), Label(top, relief=RAISED), Label(top, relief=RAISED), Label(top, relief=RAISED)],
+            [Label(top, relief=RAISED), Label(top, relief=RAISED), Label(top, relief=RAISED), Label(top, relief=RAISED)]  
+        ]
+
+labelsValues = [[48,49,50,51],[52,53,54,55]]
+
+j = 0
+for lists in labels:
+    i = 0
+    for label in lists:
+        label.grid(row = i, column = j)
+        label.textvariable = var[j][i]
+        i += 1
+    j += 1
+
+
+def incrementList(oldValues):
+    newValues = oldValues
+    i = 0
+    for row in oldValues:
+        j = 0
+        for entry in row:
+            newValues[i][j] = entry + 1
+            j += 1
+        i += 1
+    print(newValues)
+    return newValues
+
+def buttonFunction():
+    #messagebox.showinfo( "Updated.")
+    global var
+    global labelsValues
+
+    labelsValues = incrementList(labelsValues)
+    print(labelsValues)
+
+    i = 0
+    j = 0
+    for row in var:
+        for item in row:
+            item.set(chr(labelsValues[i][j]))
+            j += 1
+        i += 1
+        j = 0
+
+
+
+B = tkinter.Button(top, text ="Next", command = buttonFunction)
+
+B.grid(row = i)
+top.mainloop()
