c0f3c6d37c84fc948365a24e4b6f5cbf49150b2a
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-04-02 10:41:35 +0200

    updated histogram with custom bins

--- histogram.py
+++ histogram.py
@@ -4,12 +4,19 @@ from matplotlib import pyplot as plt
 data = pd.read_csv('exports\\XR_DATA1.csv', header = 0, sep=';')
 
 print(data.columns)
+selectedColumn = 'IB 512'
 
-
-beans = pd.unique(data['IB 515'])
+beans = pd.unique(data[selectedColumn])
+definedBins = []
 
 print(beans.tolist())
-plt.hist(data['IB 515'])
+for index, entry in enumerate(beans.tolist()):
+    definedBins.append(int(str(entry).strip()))
+
+definedBins.sort()
+print(definedBins)
+
+plt.hist(data[selectedColumn], bins = definedBins)
 
 plt.show()
 
