8f3dad3f6f8ad5f5f54c3a2c86fb2d5f03fc9d74
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2020-04-17 16:52:33 +0200

    Added explanations

--- telegramViewer/transposed-export.py
+++ telegramViewer/transposed-export.py
@@ -4,6 +4,7 @@ import re
 data = pd.read_csv('data/XR_DATA3.csv', sep = ';')
 regexString = "^((I|Q)B\s5)(([2-3][0-9])|(1[5-9]))"
 
+# Convert all the values for IB 515-531 and QB 515-531 to ASCII characters (checking is done via regex)
 for cols in data :
     newList = []
     if (re.match(regexString, cols)) :
@@ -12,6 +13,6 @@ for cols in data :
         newData = pd.DataFrame({cols : newList})
         data.update(newData)
 
-
+# Save the transposed dataFrame to an excel file
 data_transposed = data.transpose()
 data_transposed.to_excel("output.xlsx")
